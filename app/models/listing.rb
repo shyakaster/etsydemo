0cc9eac1c1fced5699a0c395fec1ecd8d9857840
alex shyaka <shyakaster@gmail.com>
2016-07-24 13:09:04 +0300

    add custom css and bootstrap

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,4 @@
 class Listing < ActiveRecord::Base
+	  has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default_image.png"
+	  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
