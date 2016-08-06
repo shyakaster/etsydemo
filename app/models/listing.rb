3bf7b33c41940cf0f989db6f570971217ee3fafc
alex shyaka <shyakaster@gmail.com>
2016-08-06 16:26:31 +0300

    add name display in listing page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,29 +1,20 @@
 class Listing < ActiveRecord::Base
-=begin
-	if Rails.env.development?
-	  has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default_image.png"
-	else
-		has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default_image.png",
-	                     #validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
-											:storage => :dropbox,
-											:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-											:path => ":style/:id_:filename"
-		end
-
-=end
 	if Rails.env.production?
 		has_attached_file :image,
 											:styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default_image.jpg",
 											:storage => :dropbox,
 											:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 											:path => ":style/:id_:filename"
-		                  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+		validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 	else
 		has_attached_file :image,
 											:styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default_image.jpg"
+		validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 	end
 
 	validates :name, :description, :price, presence: true
 	validates :price, numericality: {greater_than: 0}
 	validates_attachment_presence :image
+
+	belongs_to :user
 end
