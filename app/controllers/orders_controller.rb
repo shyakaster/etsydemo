c07f8cde6866c64b80248e563f4071008c0c8826
alex shyaka <shyakaster@gmail.com>
2016-08-09 11:05:48 +0300

    add sales and purchases history

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,16 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
+  before_filter :authenticate_user!
+
+  def sales
+    @orders = Order.all.where(seller: current_user).order('created_at DESC')
+  end
+
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order('created_at DESC')
+  end
+
   # GET /orders
   # GET /orders.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
 
   get 'seller' => 'listings#seller'
 
+  get 'sales' => 'orders#sales'
+
+  get 'purchases' => 'orders#purchases'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
